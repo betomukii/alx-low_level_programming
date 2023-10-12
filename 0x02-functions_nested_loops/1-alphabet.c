82b3f1b0e1f584f7990a439bce37dad78eadf592
betomukii <mukiibeatrice4@gmail.com>
2023-10-12 21:19:22 +0300

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabets in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	       _putchar(i);
+
+	_putchar('\n');
+}
