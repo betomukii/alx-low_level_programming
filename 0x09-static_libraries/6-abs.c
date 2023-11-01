9f597c9cf8292e49624272cddf6b5bcae3c08950
betomukki <mukiibeatrice4@gmail.com>
2023-11-01 18:18:59 +0300

    CreaA library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of a number
+* @a: function parameter
+* Return: n or -n;
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
