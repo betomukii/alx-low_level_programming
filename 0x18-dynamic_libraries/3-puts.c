b37f7a12b6f65ade1833dc1a440fb59f2a44ab18
betomukii <mukiibeatrice4@gmail.com>
2024-01-15 15:49:04 +0300

    created dynamic library

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function that compares 2 strings
+ * @s1: parameter one
+ * @S2: parameter two
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
